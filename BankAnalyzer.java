0f79ef269e8943c25fb6603fdf5dec7db81122a2
nghson <son.hngy@gmail.com>
2023-07-04 23:50:22 +0300

    simple version

--- BankAnalyzer.java
+++ BankAnalyzer.java
@@ -1,2 +1,16 @@
 public class BankAnalyzer {
+	private static final String DATA = "./";
+
+	public static void main(final String... args) throws IOException {
+		final Path path = Paths.get(DATA + args[0]);
+		final List<String> lines = Files.readAllLines(path);
+		double total = 0d;
+		for (final String line: lines) {
+			final String[] columns = line.split(",");
+			final double amount = Double.parseDouble(columns[1]);
+			total += amount;
+		}
+
+		System.out.println("Transaction total: " + total);
+	}
 }
