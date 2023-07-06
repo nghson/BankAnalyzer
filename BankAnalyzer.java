b378b65227447a10965848ec06e815ec10b250aa
nghson <son.hngy@gmail.com>
2023-07-06 23:22:39 +0300

    separate parser

--- BankAnalyzer.java
+++ BankAnalyzer.java
@@ -1,16 +1,44 @@
+package bankanalyzer;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class BankAnalyzer {
-	private static final String DATA = "./";
+	private static final String DATA_DIR = "./bankanalyzer/";
 
 	public static void main(final String... args) throws IOException {
-		final Path path = Paths.get(DATA + args[0]);
+		final Parser parser = new Parser();
+		final Path path = Paths.get(DATA_DIR + args[0]);
 		final List<String> lines = Files.readAllLines(path);
-		double total = 0d;
-		for (final String line: lines) {
-			final String[] columns = line.split(",");
-			final double amount = Double.parseDouble(columns[1]);
-			total += amount;
-		}
+		
+		final List<BankTransaction> bankTransactions
+				= parser.parseLines(lines);
 
-		System.out.println("Transaction total: " + total);
+		System.out.println("Transaction total: " + getTotal(bankTransactions));
+		System.out.println("Transaction in Jan: " + getMonth(bankTransactions, Month.JANUARY));
+	}
+
+	public static double getTotal(final List<BankTransaction> bankTransactions) {
+		double total = 0d;
+		for (final BankTransaction bankTransaction: bankTransactions) {
+			total += bankTransaction.getAmount();
+		}
+		return total;
+	}
+
+	public static List<BankTransaction> getMonth(final List<BankTransaction> bankTransactions, final Month month) {
+		final List<BankTransaction> result = new ArrayList<>();
+		for (final BankTransaction bankTransaction: bankTransactions) {
+			if (bankTransaction.getDate().getMonth() == month) {
+				result.add(bankTransaction);
+			}
+		}
+		return result;
 	}
 }
